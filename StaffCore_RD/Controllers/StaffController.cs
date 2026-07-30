2f0e76af6a5cb50bcf7c67568b1597011eb6e2c0
Dubenny Castillo <dubenny007@gmail.com>
2026-07-30 10:38:03 -0400

    StaffController con Index y Create
    - Controllers/StaffController.cs creado, con [Authorize] a nivel de clase
    - Index: lista el personal activo (Where Activo) ordenado por Nombre
    - Create GET: devuelve la vista con new Staff() para pre-enlazar campos vacios, restringido a Administrador y RRHH
    - Create POST: valida ModelState, si es valido guarda con SaveChangesAsync y redirige al Index con TempData["Exito"], restringido a Administrador y RRHH

---
+++ StaffCore_RD/Controllers/StaffController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StaffCoreRD.Data;
+using StaffCoreRD.Models;
+
+namespace StaffCoreRD.Controllers
+{
+    [Authorize]
+    public class StaffController : Controller
+    {
+        private readonly StaffDbContext _context;
+
+        public StaffController(StaffDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Staff
+        public async Task<IActionResult> Index()
+        {
+            var lista = await _context.Personal
+                .Where(s => s.Activo)
+                .OrderBy(s => s.Nombre)
+                .ToListAsync();
+
+            return View(lista);
+        }
+
+        // GET: Staff/Create
+        [Authorize(Roles = "Administrador,RRHH")]
+        public IActionResult Create()
+        {
+            return View(new Staff());
+        }
+
+        // POST: Staff/Create
+        [HttpPost]
+        [Authorize(Roles = "Administrador,RRHH")]
+        public async Task<IActionResult> Create(Staff staff)
+        {
+            if (!ModelState.IsValid)
+                return View(staff);
+
+            _context.Personal.Add(staff);
+            await _context.SaveChangesAsync();
+
+            TempData["Exito"] = "Empleado creado correctamente.";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
